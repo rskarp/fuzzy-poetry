a0732c5949fbbade70ac7365e6312e2bba95eb38
rskarp <rkarp1997@gmail.com>
2023-05-23 00:26:16 -0400

    add graphql

---
+++ src/graphql/mutations.ts
@@ -0,0 +1,60 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createPoemVariation = /* GraphQL */ `
+  mutation CreatePoemVariation(
+    $input: CreatePoemVariationInput!
+    $condition: ModelPoemVariationConditionInput
+  ) {
+    createPoemVariation(input: $input, condition: $condition) {
+      id
+      original_text
+      variation_text
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+    }
+  }
+`;
+export const updatePoemVariation = /* GraphQL */ `
+  mutation UpdatePoemVariation(
+    $input: UpdatePoemVariationInput!
+    $condition: ModelPoemVariationConditionInput
+  ) {
+    updatePoemVariation(input: $input, condition: $condition) {
+      id
+      original_text
+      variation_text
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+    }
+  }
+`;
+export const deletePoemVariation = /* GraphQL */ `
+  mutation DeletePoemVariation(
+    $input: DeletePoemVariationInput!
+    $condition: ModelPoemVariationConditionInput
+  ) {
+    deletePoemVariation(input: $input, condition: $condition) {
+      id
+      original_text
+      variation_text
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+    }
+  }
+`;
+export const generatePoemVariation = /* GraphQL */ `
+  mutation GeneratePoemVariation($originalPoem: String) {
+    generatePoemVariation(originalPoem: $originalPoem)
+  }
+`;
