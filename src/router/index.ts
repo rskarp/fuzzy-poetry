ee186bab7c91b46e5ac342df7a64449afa4a053e
rskarp <rkarp1997@gmail.com>
2023-06-12 03:12:21 -0400

    fix view imports

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import ContactView from '@/views/ContactView.vue'
-import HowItWorksView from '@/views/HowItWorksView.vue'
-import AboutView from '@/views/AboutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,17 +15,17 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => AboutView
+      component: () => import('../views/AboutView.vue')
     },
     {
       path: '/contact',
       name: 'contact',
-      component: () => ContactView
+      component: () => import('../views/ContactView.vue')
     },
     {
       path: '/how-it-works',
       name: 'how-it-works',
-      component: () => HowItWorksView
+      component: () => import('../views/HowItWorksView.vue')
     }
   ]
 })
