71e880005cbb91702b7a7c12f7908baac27e4fe4
rskarp <rkarp1997@gmail.com>
2023-06-11 23:09:18 -0400

    replace spacy w nltk

--- amplify/backend/function/generatePoemVariation/src/index.py
+++ amplify/backend/function/generatePoemVariation/src/index.py
@@ -2,33 +2,64 @@ import json
 import boto3
 import os
 from uuid import uuid4
-import spacy
+# import spacy
 import datamuse
+import nltk
 from random import sample
 # # import en_core_web_md
 from string import punctuation
+from nltk.tokenize import word_tokenize, sent_tokenize
+
+nltk.download('averaged_perceptron_tagger')
+nltk.download('punkt')
+nltk.download('universal_tagset')
 
 client = boto3.client("dynamodb", 'us-east-1')
 # os.environ.get("STORAGE_POEMVARIATIONTABLE_NAME")
-TABLE = 'PoemVariation-spjf5e27hnh5bihsf7agym7vva-staging'
+# TABLE = 'PoemVariation-spjf5e27hnh5bihsf7agym7vva-staging'
+TABLE = 'PoemVariation-kvrbuteftvd5xofbsmnb4qb2lm-develop'
+
+
+# def get_tokens_spacy(text):
+#     # nlp = en_core_web_md.load()
+#     try:
+#         nlp = spacy.load('en_core_web_md')
+#     except OSError:
+#         from spacy.cli import download
+#         download('en_core_web_md')
+#         nlp = spacy.load('en_core_web_md')
+#     doc = nlp(text)
+#     tokens = [token for token in doc]
+#     content_tokens = [token for token in doc if token in [
+#         'NOUN', 'VERB', 'ADJ', 'ADV']]
+
+#     return tokens, content_tokens
 
 
 def get_tokens(text):
-    # nlp = en_core_web_md.load()
-    try:
-        nlp = spacy.load('en_core_web_md')
-    except OSError:
-        from spacy.cli import download
-        download('en_core_web_md')
-        nlp = spacy.load('en_core_web_md')
-    doc = nlp(text)
-    tokens = [token for token in doc]
-    content_tokens = [token for token in doc if token.pos_ in [
+    tokens = []
+    for sent in sent_tokenize(text, language='english'):
+        wordtokens = word_tokenize(sent, language='english')
+        tokens.extend(nltk.pos_tag(wordtokens, tagset='universal'))
+    content_tokens = [token for token in tokens if token[1] in [
         'NOUN', 'VERB', 'ADJ', 'ADV']]
     return tokens, content_tokens
 
 
-def spacyPOS_to_datamusePOS(pos):
+# def spacyPOS_to_datamusePOS(pos):
+#     if pos == 'NOUN':
+#         return 'n'
+#     elif pos == 'VERB':
+#         return 'v'
+#     elif pos == 'ADJ':
+#         return 'adj'
+#     elif pos == 'ADV':
+#         return 'adv'
+#     else:
+#         return ''
+
+
+def nltk_to_datamusePOS(pos):
     if pos == 'NOUN':
         return 'n'
     elif pos == 'VERB':
@@ -44,7 +75,7 @@ def spacyPOS_to_datamusePOS(pos):
 def get_candidates(token, replacement_types, max_results=50):
     # print(f'Replacing {token.text}...')
     dm = datamuse.Datamuse()
-    text = token.text.strip(punctuation)
+    text = token[0].strip(punctuation)
     ml = text if 'ml' in replacement_types else None
     sp = f'//{text}' if 'ana' in replacement_types else text if 'sp' in replacement_types else None
     rel_trg = text if 'rel_trg' in replacement_types else None
@@ -55,7 +86,7 @@ def get_candidates(token, replacement_types, max_results=50):
     options = dm.words(md='p', ml=ml, sp=sp, rel_trg=rel_trg,
                        rel_cns=rel_cns, rel_hom=rel_hom, max=max_results)
     # Filter words by matching part of speech
-    return [o for o in options if 'tags' in o and spacyPOS_to_datamusePOS(token.pos_) in o['tags']]
+    return [o for o in options if 'tags' in o and nltk_to_datamusePOS(token[1]) in o['tags']]
 
 
 def createPoemVariation(text):
@@ -71,21 +102,21 @@ def createPoemVariation(text):
     to_replace = sample(content_tokens, number_to_replace)
     print(f'Proccessing {len(tokens)} tokens')
     for i, token in enumerate(tokens):
-        newWord = token.text
+        newWord = token[0]
         if token in to_replace:
             options = get_candidates(token, replacement_types, max_options)
             # print(f'Num candidates: {len(options)}')
             if len(options) <= 1:
                 print(
-                    f'Num candidates: {len(options)}, word: {token.text}, type: {",".join(replacement_types)}')
+                    f'Num candidates: {len(options)}, word: {token[0]}, type: {",".join(replacement_types)}')
             if len(options) > 0:
                 # print(len(options))
                 chosen = sample(options, 1)
-                newWord = f'{chosen[0]["word"]}[#ORIGINAL_{token.text}]'
+                newWord = f'{chosen[0]["word"]}[#ORIGINAL_{token[0]}]'
             else:
-                newWord = f'{token.text}[#ORIGINAL_{token.text}]'
+                newWord = f'{token[0]}[#ORIGINAL_{token[0]}]'
 
-        out_words.append(f'{newWord}{token.whitespace_}')
+        out_words.append(f'{newWord} ')
 
     poem = ''.join(out_words).replace('\n\n', '\n')
     return poem
@@ -106,4 +137,4 @@ def handler(event, context):
 
 if __name__ == '__main__':
     print(createPoemVariation(
-        '''Mary had a little lamb, little lamb, little lamb'''))
+        '''Mary had a little lamb, little lamb, little lamb. Mary had a little lamb whose fleese was white as snow'''))
