85c7c0211be6c91331ae8880ef3ce64d9063e5f0
Riley Karp <rkarp1997@gmail.com>
2025-08-05 01:19:08 -0400

    Fix/appsync-unauthorized-error (#20)
    * Feature/homepage (#13)
    
    * create email endpoint & call it from contant page
    
    * update sendEmail lambda
    
    * add maya, mercedes, presley bios
    
    * fix mercedes bio
    
    * update homepage
    
    * Feature/home-styles (#15)
    
    * create email endpoint & call it from contant page
    
    * update sendEmail lambda
    
    * add maya, mercedes, presley bios
    
    * fix mercedes bio
    
    * update homepage
    
    * edit homepage styles
    
    * fix header/footer styles
    
    * update colors
    
    * add version 3 placeholder
    
    * add finetune v3 model name
    
    * change nltk download dir
    
    * add nltk downloads
    
    * homepage adjustments
    
    * update apsync key exp
    
    * amplify update api
    
    * update appsync config
    
    * staging env

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -14,7 +14,7 @@ const awsmobile = {
     'https://3xspzuyfejfungozfk2brgzcpy.appsync-api.us-east-1.amazonaws.com/graphql',
   aws_appsync_region: 'us-east-1',
   aws_appsync_authenticationType: 'API_KEY',
-  aws_appsync_apiKey: 'da2-ijg24ln7tzfnnfh7nmmq4a6b7q'
+  aws_appsync_apiKey: 'da2-irmy2rvbjvhgfcopfi2nwnmhom'
 }
 
 export default awsmobile
--- src/models/index.d.ts
+++ src/models/index.d.ts
@@ -1,47 +1,44 @@
-import { ModelInit, MutableModel, __modelMeta__, ManagedIdentifier } from '@aws-amplify/datastore'
+import { ModelInit, MutableModel, __modelMeta__, ManagedIdentifier } from "@aws-amplify/datastore";
 // @ts-ignore
-import { LazyLoading, LazyLoadingDisabled } from '@aws-amplify/datastore'
+import { LazyLoading, LazyLoadingDisabled } from "@aws-amplify/datastore";
 
 export enum ReplacementType {
-  MEANS_LIKE = 'MEANS_LIKE',
-  TRIGGERED_BY = 'TRIGGERED_BY',
-  ANAGRAM = 'ANAGRAM',
-  SPELLED_LIKE = 'SPELLED_LIKE',
-  CONSONANT_MATCH = 'CONSONANT_MATCH',
-  HOMOPHONE = 'HOMOPHONE'
+  MEANS_LIKE = "MEANS_LIKE",
+  TRIGGERED_BY = "TRIGGERED_BY",
+  ANAGRAM = "ANAGRAM",
+  SPELLED_LIKE = "SPELLED_LIKE",
+  CONSONANT_MATCH = "CONSONANT_MATCH",
+  HOMOPHONE = "HOMOPHONE"
 }
+
+
 
 type EagerPoemVariation = {
   readonly [__modelMeta__]: {
-    identifier: ManagedIdentifier<PoemVariation, 'id'>
-    readOnlyFields: 'createdAt' | 'updatedAt'
-  }
-  readonly id: string
-  readonly original_text?: string | null
-  readonly variation_text?: string | null
-  readonly createdAt?: string | null
-  readonly updatedAt?: string | null
+    identifier: ManagedIdentifier<PoemVariation, 'id'>;
+    readOnlyFields: 'createdAt' | 'updatedAt';
+  };
+  readonly id: string;
+  readonly original_text?: string | null;
+  readonly variation_text?: string | null;
+  readonly createdAt?: string | null;
+  readonly updatedAt?: string | null;
 }
 
 type LazyPoemVariation = {
   readonly [__modelMeta__]: {
-    identifier: ManagedIdentifier<PoemVariation, 'id'>
-    readOnlyFields: 'createdAt' | 'updatedAt'
-  }
-  readonly id: string
-  readonly original_text?: string | null
-  readonly variation_text?: string | null
-  readonly createdAt?: string | null
-  readonly updatedAt?: string | null
+    identifier: ManagedIdentifier<PoemVariation, 'id'>;
+    readOnlyFields: 'createdAt' | 'updatedAt';
+  };
+  readonly id: string;
+  readonly original_text?: string | null;
+  readonly variation_text?: string | null;
+  readonly createdAt?: string | null;
+  readonly updatedAt?: string | null;
 }
 
-export declare type PoemVariation = LazyLoading extends LazyLoadingDisabled
-  ? EagerPoemVariation
-  : LazyPoemVariation
+export declare type PoemVariation = LazyLoading extends LazyLoadingDisabled ? EagerPoemVariation : LazyPoemVariation
 
 export declare const PoemVariation: (new (init: ModelInit<PoemVariation>) => PoemVariation) & {
-  copyOf(
-    source: PoemVariation,
-    mutator: (draft: MutableModel<PoemVariation>) => MutableModel<PoemVariation> | void
-  ): PoemVariation
+  copyOf(source: PoemVariation, mutator: (draft: MutableModel<PoemVariation>) => MutableModel<PoemVariation> | void): PoemVariation;
 }
