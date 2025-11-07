e139274c49816add7634a7f6d614c8c51180a0fc
rskarp <rkarp1997@gmail.com>
2025-11-07 00:36:34 -0500

    add iam policies

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -14,6 +14,13 @@ export type AmplifyDependentResourcesAttributes = {
       "Name": "string",
       "Region": "string"
     },
+    "generateMultimodalVariation": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
     "generatePoemVariation": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
