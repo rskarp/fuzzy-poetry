f0372bf6bf246550e3363a764672fa9e21ff15b8
rskarp <rkarp1997@gmail.com>
2025-11-12 02:02:02 -0500

    update README & eslint

---
+++ README.md
@@ -0,0 +1,3 @@
+# Fuzzy Poetry
+
+This is an ongoing project being done in collaboration with Professor Gianluca Rizzo, Paganucci Associate Professor of Italian Language and Literature at Colby College, to create a natural language processing tool to aid poetry writing. For more information and to try a web version of our tool, visit [fuzzypoetry.com](https://www.fuzzypoetry.com/).
--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,6 +2,8 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import reactX from 'eslint-plugin-react-x'
+import reactDom from 'eslint-plugin-react-dom'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
@@ -14,10 +16,16 @@ export default defineConfig([
       tseslint.configs.recommendedTypeChecked,,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      reactX.configs['recommended-typescript'],
+      reactDom.configs.recommended,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
   },
 ])
